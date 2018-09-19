8dad060a63dcef894d7e036458e131799b3b2c7a
Oliver Dudman <oliver.dudman@icloud.com>
2018-09-19 23:25:28 +0100

    remove unused code from gulpfile and change dist destination

--- homepage/gulpfile.js
+++ homepage/gulpfile.js
@@ -3,16 +3,12 @@ const browserSync = require("browser-sync");
 const plumber = require("gulp-plumber");
 const reload = browserSync.reload;
 const sass = require("gulp-sass");
-const browserify = require("browserify");
-const source = require("vinyl-source-stream");
 const sourcemaps = require("gulp-sourcemaps");
 const notify = require("gulp-notify");
 const rename = require("gulp-rename");
-const envify = require("envify/custom");
 const htmlreplace = require("gulp-html-replace");
 const clean = require("gulp-clean");
 const htmlmin = require("gulp-htmlmin");
-const exorcist = require("exorcist");
 
 const onError = function(err) {
   notify.onError({
@@ -29,15 +25,6 @@ gulp.task("sass", function() {
   .pipe(sass())
   .pipe(sourcemaps.write())
   .pipe(gulp.dest("css"))
-  .pipe(reload({stream: true}));
-});
-
-gulp.task("browserify", function() {
-  return browserify("js/src/main.js")
-  .transform("babelify", {presets: ["@babel/preset-env", "@babel/preset-react"]})
-  .bundle()
-  .pipe(source("main.js"))
-  .pipe(gulp.dest("js/build"))
   .pipe(reload({stream: true}));
 });
 
@@ -64,7 +51,7 @@ gulp.task("build_html", function() {
     "js": "js/main.min.js"
   }))
   .pipe(htmlmin({collapseWhitespace: true}))
-  .pipe(gulp.dest("dist"));
+  .pipe(gulp.dest("../docs"));
 });
 
 gulp.task("build_sass", function() {
@@ -73,25 +60,16 @@ gulp.task("build_sass", function() {
   .pipe(sourcemaps.init())
   .pipe(sass({outputStyle: "compressed"}))
   .pipe(sourcemaps.write("./"))
-  .pipe(gulp.dest("dist/css"));
-});
-
-gulp.task("build_js", function() {
-  return browserify("js/src/main.js", {debug: true})
-  .transform("babelify", {presets: ["@babel/preset-env", "@babel/preset-react"]})
-  .transform({global: true}, envify({NODE_ENV: "production"}))
-  .transform("uglifyify", {global: true})
-  .bundle()
-  .pipe(exorcist("dist/js/main.min.map.js"))
-  .pipe(source("main.min.js"))
-  .pipe(gulp.dest("dist/js"));
+  .pipe(gulp.dest("../docs/css"));
 });
 
 gulp.task("clean", function() {
-  return gulp.src("dist", {allowEmpty: true})
+  gulp.src("../docs/index.html", {allowEmpty: true})
+  .pipe(clean());
+  return gulp.src("../docs/css", {allowEmpty: true})
   .pipe(clean());
 });
 
 gulp.task("build", gulp.series("clean",
-  gulp.parallel("build_html", "build_sass", "build_js")
+  gulp.parallel("build_html", "build_sass")
 ));
